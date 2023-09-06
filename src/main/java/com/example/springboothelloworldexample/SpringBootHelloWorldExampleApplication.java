eb5b06579d2154bfcc49779ae111e454aa95c852
Ajaty <jesseossvargas@gmail.com>
2023-09-06 18:41:12 -0300

    ajuste config

--- src/main/java/com/example/springboothelloworldexample/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/example/springboothelloworldexample/SpringBootHelloWorldExampleApplication.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-
-import com.example.springboothelloworldexample.exception.UrlAccessException;
 
 @SpringBootApplication
 public class SpringBootHelloWorldExampleApplication implements CommandLineRunner{
@@ -22,14 +18,14 @@ public class SpringBootHelloWorldExampleApplication implements CommandLineRunner
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(" *** mail properties ***");
-		System.out.println(" From : "+configProperties.getFrom());
-		System.out.println(" To : "+configProperties.getTo());
+		System.out.println(" From : " + configProperties.getFrom());
+		System.out.println(" To : "+ configProperties.getTo());
 		System.out.println(" Default Object :"+configProperties.getDefaultObject());
 	}
 
-	@EventListener(classes = ContextRefreshedEvent.class)
-	public void listen() {
-		throw new UrlAccessException("http://www.example.com");
-	}
-	
+//	@EventListener(classes = ContextRefreshedEvent.class)
+//	public void listen() {
+////		throw new UrlAccessException("http://www.example.com");
+//	}
+//	
 }
